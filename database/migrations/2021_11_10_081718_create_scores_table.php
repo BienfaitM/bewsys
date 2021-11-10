2c0d9488c4d8d5823d0b7cb50ca799f33ebb5c95
Bienfait <bienfait.murhulamwambali@gmail.com>
2021-11-10 12:13:28 +0300

    second commit

---
+++ database/migrations/2021_11_10_081718_create_scores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            $table->string('Score_Name');
+            $table->integer('Values');
+            $table->unsignedBigInteger('Question_id');
+            $table->foreign('Question_id')->references('id')->on('questions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
