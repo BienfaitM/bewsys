7721534529750a9a745d38f3908e7ff071f5e4e6
Bienfait <bienfait.murhulamwambali@gmail.com>
2021-12-27 11:16:26 +0300

    all

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -49,12 +49,23 @@
         </div>
     </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-
-<script>
-    $(document).ready(function() {
-        $('#performance_table').DataTable();
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+<script type="text/javascript">
+  $(function () {
+    var table = $('.data-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('users.index') }}",
+        columns: [
+            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+            {data: 'name', name: 'Question Category'},
+            {data: 'email', name: 'Section'},
+            {data: 'action', name: 'Actions', orderable: false, searchable: false},
+        ]
     });
 </script>
 
